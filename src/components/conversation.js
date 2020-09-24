580d98b7d38f4ad6e3b426655849e8df965b442c
haditariq <hadi-tariq@live.co.uk>
2020-09-24 18:52:10 +0500

    conversation item render

--- src/components/conversation.js
+++ src/components/conversation.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import '../assets/css/conversation.css'
-import ConversationItem from './conversationItem';
-
-function Conversation (props) {
+import React from "react";
+import "../assets/css/conversation.css";
+import ConversationItem from "./conversationItem";
+import conversationData from "../datalayer/conversation";
+function Conversation(props) {
+  const renderConversation = () => {
+    return conversationData.conversation.map((item) => <ConversationItem
+      payload={item.payload}
+      username={item.username}
+      sent_by={item.sent_by}
+      sent_at={item.sent_at}
+    />);
+  };
   return (
-    <div className={'conversation'}>
+    <div className={"conversation"}>
       <div className="conversation__TopBar">
-        <p className={"conversation__username"}>
-          Micheal Wong
-        </p>
+        <p className={"conversation__username"}>Micheal Wong</p>
         <div
           className={`conversation__status ${
-            true ? "conversation__status--active" : "conversation__status--inActive"
+            true
+              ? "conversation__status--active"
+              : "conversation__status--inActive"
           }`}
         />
       </div>
-      <div className="conversationBlock">
-        <ConversationItem/>
-        <ConversationItem/>
-        <ConversationItem/>
-        <ConversationItem/>
-        <ConversationItem/>
-      </div>
+      <div className="conversationBlock">{renderConversation()}</div>
     </div>
   );
 }
--- src/components/conversationItem.js
+++ src/components/conversationItem.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "../assets/css/conversationItem.css";
 import user_profile1 from "../assets/images/user_profile1.png";
-function ConversationItem(props) {
+function ConversationItem({payload, username, sent_at, sent_by}) {
   return (
-    <div className={"conversationItem"}>
+    <div className={`conversationItem ${sent_by !== "A" ? "conversationItem__directionStart" : "conversationItem__directionEnd"}`}>
       <img src={user_profile1} alt="" className={"conversationItem__photo"} />
       <div className={"conversationItem__payloadContainer"}>
-        <p className={"conversationItem__info"}>username, 7:30 PM</p>
-        <p className="conversationItem__payload">
-          I agree! It definitely produces a better user experience.
+        <p className={"conversationItem__info"}>{username}, {sent_at}</p>
+        <p className={`conversationItem__payload ${sent_by !== "A" ? "conversationItem__payloadDirectionStart" : "conversationItem__payloadDirectionEnd"}`}>
+          {payload}
         </p>
       </div>
     </div>
